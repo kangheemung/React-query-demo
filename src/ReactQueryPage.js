7349b83a29ed345e451a1184d6a75920ee1ddbad
kurotamashi30 <kurotamashi30@gmail.com>
2024-08-29 17:33:10 +0900

    id

--- src/ReactQueryPage.js
+++ src/ReactQueryPage.js
@@ -5,11 +5,14 @@ import React from 'react';
 //dataを見る
 
 const ReactQueryPage = () => {
-    const fetchPost=()=>{
-        return axios.get('http://localhost:3004/posts')
+    const fetchPost=(queryData)=>{
+        console.log(queryData);
+        const id=queryData.queryKey.find((key) => key.id).id;;
+        return axios.get(`http://localhost:3004/posts/${id}`)
     }
-    const {isLoading,data,isError,error} = useQuery({
-    queryKey:["posts"],//各々のAPIの名前をきめる
+    const {isLoading,data,isError,error,refetch} = useQuery({
+    // queryKey:["posts"],//各々のAPIの名前をきめる
+    queryKey:["posts",{id: 1}],//상세페이지
     //각각의 API의 이름을 정한다.
     queryFn: fetchPost,
     retry: 1,
@@ -17,8 +20,12 @@ const ReactQueryPage = () => {
     //api를 부를때의 시간 /staleTime< gcTime
     select:(data)=>{
         return data.data;
-    },
-        gcTime: 5000,//기본값 5분 캐쉬 가져가는 시간
+    },// gcTime: 5000,//기본값 5분 캐쉬 가져가는 시간
+        //refetchInterval:3000,
+        //refetchOnMount:false,// 호출 한번만 하겠다. 
+        // refetchOnWindowFocus:true//윈도우가 다시 이창으로 돌아오면 다시 리프레쉬해주기.
+        enabled: false,// 호출이 안됨사용할수 없음 버튼을 눌렀을때 호출되게 사용할수 있다.조건을 써줄수도 있다.기본값은 참
+
     });
     console.log("ddd",data,isLoading);
     console.log("earror",isError,error);
@@ -31,10 +38,12 @@ const ReactQueryPage = () => {
         return <h1>{error.message}</h1>;
     }
     return(
-    <div>
-        {data.map((item)=>(
-    <div>{item.title}</div>
-    ))}
+   <div>
+    {/* {data?.map((item)=>( */}
+    {/*  <div>{item.title}</div> */}
+ 
+    {/*  ))} */}
+     <button onClick={refetch}>post리스트 다시 들고 오기</button>
     </div>
     );
 
